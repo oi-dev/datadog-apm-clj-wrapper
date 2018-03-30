9295423fe348f89f57990d1ead998edf7a9e052f
Shobana M <shobanam@thoughtworks.com>
2018-03-30 10:59:45 +0530

    [Mrinal/Shobana]Adding README

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject datadog-apm-clj-wrapper "0.1.0"
-  :description "This project wraps the java implementation for datadog apm"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject datadog-apm-clj-wrapper "0.1.1"
+            :description "This project gives a way to use the datadog-apm tracing in clojure"
+            :url "https://github.com/oi-dev/datadog-apm-clj-wrapper"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :signing {:gpg-key "796FCFED"
-            :gpg-passphrase :env/gpgpass}
+            :signing {:gpg-key "796FCFED"
+                      :gpg-passphrase :env/gpgpass}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [environ "1.0.3"]
-                 [com.datadoghq/dd-trace-ot "0.5.0"]
-                 [io.opentracing/opentracing-api "0.31.0"]
-                 [io.opentracing/opentracing-util "0.31.0"]])
+            :dependencies [[org.clojure/clojure "1.8.0"]
+                           [environ "1.0.3"]
+                           [com.datadoghq/dd-trace-ot "0.5.0"]
+                           [io.opentracing/opentracing-api "0.31.0"]
+                           [io.opentracing/opentracing-util "0.31.0"]])
